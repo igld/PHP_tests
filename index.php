f536fb4d6a0060dd8be8c392e85223cf08f6bc11
igl <surf_gagnant@hotmail.fr>
2018-05-11 11:27:35 +0200

    test de la condition if

--- index.php
+++ index.php
@@ -3,5 +3,18 @@
 Si vous ne me croyez pas...
 ... vous avez raison ;o) */
 echo "J'ai 46 ans je suis David.";
+
+
+
+<?php
+$age = 46;
+
+if ($age >= 46)
+{
+    echo "Salut gamin !";
+}
+
+
+
 phpinfo();
 ?>
